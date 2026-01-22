f96071dc0846b728354682cba1ed9e6494a151bd
Jyoti <123319919+21Jyoti21@users.noreply.github.com>
2026-01-22 11:58:22 +0530

    Update ApiRoutes.js

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,17 +1,23 @@
-export const HOST="http://localhost:3005";
-const AUTH_ROUTE=`${HOST}/api/auth`;
-const MESSAGES_ROUTE=`${HOST}/api/messages`;
-const CALLS_ROUTE = `${HOST}/api/calls`;
+// ✅ NEVER hardcode localhost here
+export const HOST = process.env.NEXT_PUBLIC_HOST;
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const CHECK_USER_ROUTE=`${AUTH_ROUTE}/check-user`;
-export const ONBOARD_USER_ROUTE=`${AUTH_ROUTE}/onboard-user`;
-export const GET_ALL_CONTACTS=`${AUTH_ROUTE}/get-contacts`;
-export const GET_CALL_TOKEN =`${AUTH_ROUTE}/generate-token`;
+// Auth
+const AUTH_ROUTE = `${API_URL}/api/auth`;
+export const CHECK_USER_ROUTE = `${AUTH_ROUTE}/check-user`;
+export const ONBOARD_USER_ROUTE = `${AUTH_ROUTE}/onboard-user`;
+export const GET_ALL_CONTACTS = `${AUTH_ROUTE}/get-contacts`;
+export const GET_CALL_TOKEN = `${AUTH_ROUTE}/generate-token`;
 
-export const ADD_MESSAGE_ROUTE=`${MESSAGES_ROUTE}/add-message`;
-export const GET_MESSAGES_ROUTE=`${MESSAGES_ROUTE}/get-messages`;
-export const ADD_IMAGE_MESSAGE_ROUTE=`${MESSAGES_ROUTE}/add-image-message`;
-export const ADD_AUDIO_MESSAGE_ROUTE=`${MESSAGES_ROUTE}/add-audio-message`;
-export const GET_INITIAL_CONTACTS_ROUTE=`${MESSAGES_ROUTE}/get-initial-contacts`;
+// Messages
+const MESSAGES_ROUTE = `${API_URL}/api/messages`;
+export const ADD_MESSAGE_ROUTE = `${MESSAGES_ROUTE}/add-message`;
+export const GET_MESSAGES_ROUTE = `${MESSAGES_ROUTE}/get-messages`;
+export const ADD_IMAGE_MESSAGE_ROUTE = `${MESSAGES_ROUTE}/add-image-message`;
+export const ADD_AUDIO_MESSAGE_ROUTE = `${MESSAGES_ROUTE}/add-audio-message`;
+export const GET_INITIAL_CONTACTS_ROUTE = `${MESSAGES_ROUTE}/get-initial-contacts`;
+
+// Calls
+const CALLS_ROUTE = `${API_URL}/api/calls`;
 export const ADD_CALL_LOG_ROUTE = `${CALLS_ROUTE}/add-log`;
 export const GET_CALL_LOGS_ROUTE = `${CALLS_ROUTE}/get-logs`;
